7ac6ddce0616fd11a467cb690f5642ec01c47e52
Eddie956 <mutugieddie3@gmail.com>
2018-08-24 08:29:52 +0300

    add the changes

---
+++ credential.py
@@ -0,0 +1,13 @@
+
+class User:
+
+	user_list = []
+
+	def __int__(self,f_name,l_name,password):
+		'''
+		methods that defines properties for each
+		'''
+		self.f_name = f_name
+		self.l_name = l_name
+		self.password = password
+
