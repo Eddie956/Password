bde74bb62627c5e273215fa349c167e42c41e3d6
Eddie956 <mutugieddie3@gmail.com>
2018-08-26 13:22:44 +0300

    just created a the main function

--- run.py
+++ run.py
@@ -46,4 +46,35 @@ def copy_credential(account_name):
     function to copy credential details in a clipboard
     '''
     return Credential.copy credential(account_name)
-    
+
+def main():
+    print("Hello Welcome to Password locker.what is your name?")
+    user_name = input()
+    print(f"Hello {user_name}. what would you like to do?")
+    print('\n')
+
+    while Ttrue:
+        print("use these short codes : ca - create account, lg - login, dc - display credential, fc - find credential, ex - exit")
+
+        short_code = input().lower()
+        if short_code == 'ca':
+            print('Create a new account:')
+            f_name = input('Enter your first name -').strip()
+            l_name = input('Enter your last name -').strip()
+            password = ('Enter your password - ').strip()
+            save_user(create_user(f_name,l_name,password))
+            print("\n")
+            print(f"New Account Created for: {f_name} ")
+        
+        elif short_code == 'lg':
+            print("Please login to your acount:")
+            user_name = input('Enter your first name - ').strip()
+            password = str(input('Enter your password - '))
+            user_exists = verify_user(user_name,password)
+            if user_exists == user_name:
+                print("\n")
+                print("Welcome {user_name}. What do you want to do")
+                
+                while True:
+                    print
+                
