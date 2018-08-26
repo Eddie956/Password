61fef7cb5d7a0550c8589fb8c0b6ce0cc31e162c
Eddie956 <mutugieddie3@gmail.com>
2018-08-26 10:42:23 +0300

    created a function to generate a passord for credentials created a function to create new credentials created afunction to save the credential object passed in the parameter

--- run.py
+++ run.py
@@ -1,1 +1,37 @@
 #!/usr/bin/env python3.6
+import pyperclip
+from credential import User,Credential
+
+def create_user(f_name,l_name,password):
+    '''
+    function that create a new user
+    '''
+    new_user = User(f_name,l_name,password)
+    return new_user
+
+def save_user(user):
+    '''
+    saving a new user acount
+    '''
+    User.save_user(user)
+
+def generate_password():
+    '''
+    to generate a password automaticaly
+    '''
+    generate_password = Credential.generate_password()
+    return generate_password
+
+def create_credential(user_name,account_name,password):
+    '''
+    function to create a new credential
+    '''
+    new_credential=Credential(user_name,account_name,password)
+    return new_credential
+
+def save_credential(credential):
+    '''
+    functions to save credentials
+    '''
+    Credential.save_credential(credential)
+    
